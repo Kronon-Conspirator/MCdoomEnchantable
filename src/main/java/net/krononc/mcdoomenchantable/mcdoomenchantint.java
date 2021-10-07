637e0d85c869ead856085f07cdcd5a15516bc24e
KrononConspirator <ApexConspirator_367@outlook.com>
2021-10-07 16:53:26 +0800

    Added Mixin for BaseItem (guns essentially)

--- src/main/java/net/krononc/mcdoomenchantable/mcdoomenchantint.java
+++
@@ -1,9 +0,0 @@
-package net.krononc.mcdoomenchantable;
-
-import net.fabricmc.api.ModInitializer;
-
-public class mcdoomenchantint implements ModInitializer {
-	@Override
-	public void onInitialize() {
-	}
-}
