dd63ac9ee192e9ad005be8c88fc85785dfdb294f
KrononConspirator <ApexConspirator_367@outlook.com>
2021-10-07 17:37:38 +0800

    Bool Fix & Additional Mixin

---
+++ src/main/java/net/krononc/mcdoomenchantable/mixin/ChainsawAnimatedMixin.java
@@ -0,0 +1,68 @@
+package net.krononc.mcdoomenchantable.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.List;
+
+import io.netty.buffer.Unpooled;
+
+
+import mod.azure.doom.DoomMod;
+import mod.azure.doom.item.weapons.ChainsawAnimated;
+import mod.azure.doom.util.enums.DoomTier;
+import mod.azure.doom.util.registry.DoomBlocks;
+//import mod.azure.doom.*;
+
+import net.minecraft.client.item.TooltipContext;
+import net.minecraft.enchantment.Enchantments;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.AxeItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ToolMaterial;
+import net.minecraft.network.PacketByteBuf;
+import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableText;
+import net.minecraft.util.Formatting;
+import net.minecraft.util.Hand;
+import net.minecraft.util.collection.DefaultedList;
+import net.minecraft.world.World;
+import software.bernie.geckolib3.core.AnimationState;
+import software.bernie.geckolib3.core.IAnimatable;
+import software.bernie.geckolib3.core.PlayState;
+import software.bernie.geckolib3.core.builder.AnimationBuilder;
+import software.bernie.geckolib3.core.controller.AnimationController;
+import software.bernie.geckolib3.core.event.predicate.AnimationEvent;
+import software.bernie.geckolib3.core.manager.AnimationData;
+import software.bernie.geckolib3.core.manager.AnimationFactory;
+import software.bernie.geckolib3.network.GeckoLibNetwork;
+import software.bernie.geckolib3.network.ISyncable;
+import software.bernie.geckolib3.util.GeckoLibUtil;
+
+
+
+@Mixin(value =  ChainsawAnimated.class)
+public class ChainsawAnimatedMixin extends Item implements IAnimatable {
+
+	public AnimationFactory factory = new AnimationFactory(this);
+	private String controllerName = "controller";
+	
+	@Override
+	public AnimationFactory getFactory() {
+		return this.factory;
+	}
+	
+	public ChainsawAnimatedMixin() {
+		super(new Item.Settings().group(DoomMod.DoomWeaponItemGroup).maxCount(1).maxDamage(601));
+	}
+
+	@Override
+	public boolean isEnchantable(ItemStack stack) {
+		return true;
+	}
+}
